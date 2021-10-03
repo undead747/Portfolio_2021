c468f6ac0e58b023f93413c8fdb5b993620bcb3d
TechCare <narutominh747@gmail.com>
2021-10-03 12:47:54 +0700

    complete validation

--- js/main.js
+++ js/main.js
@@ -1,9 +1,10 @@
+var App = {};
+
 const SideBarModule = (function () {
     const closeBtns = document.querySelectorAll('.js-side-bar-toggle');
     const sideBar = document.getElementById('aside');
     const sideBarLinks = document.querySelectorAll('.js-aside__link');
 
-    const header = document.getElementById('banner-header');
     const banner = document.getElementById('banner');
     const about = document.getElementById('about');
     const service = document.getElementById('service');
@@ -16,42 +17,43 @@ const SideBarModule = (function () {
     const portfolioLink = document.getElementById('portfolio-link');
     const contactLink = document.getElementById('contact-link');
 
+    // Click event listener for open-close sidebar buttons
     closeBtns.forEach(function (element) {
         element.addEventListener('click', function (event) {
             sideBar.classList.toggle('aside--open');
         })
     })
 
+    // Click event listener for every sidebar link 
     sideBarLinks.forEach(function (element) {
         element.addEventListener('click', function (event) {
-            window.removeEventListener('scroll', SideBarScrollAnimation);
             let sideBarLinkActive = document.querySelector('.sidebar--active');
+            let y = document.documentElement.scrollTop;
 
+            // Stop page-scroll event listener
+            window.removeEventListener('scroll', SideBarScrollAnimation);
+
+            // Remove current active side-bar link
             sideBarLinkActive.classList.remove('sidebar--active');
+            // Set active class to current event element
             event.target.classList.add('sidebar--active');
+
+            // Wait .5s and close side-bar
             setTimeout(function () {
                 sideBar.classList.toggle('aside--open');
             }, 500);
-            let y = document.documentElement.scrollTop;
+
+            // Check if Page is scrolling or not - if not active page-scroll event listener 
             var checkScroll = setInterval(function () {
                 if (y === document.documentElement.scrollTop) {
                     window.addEventListener('scroll', SideBarScrollAnimation);
                     clearInterval(checkScroll);
                 } else y = document.documentElement.scrollTop;
-            }, 300);
+            }, 500);
         })
     })
 
-    window.addEventListener('scroll', function () {
-        let y = Math.round(this.scrollY);
-
-        if (y > 0) {
-            header.classList.add('banner__header--scroll-active');
-        } else {
-            header.classList.remove("banner__header--scroll-active");
-        }
-    });
-
+    //Page scroll event listener (change side-bar active link)
     window.addEventListener('scroll', SideBarScrollAnimation);
 
     function SideBarScrollAnimation() {
@@ -91,9 +93,23 @@ const SideBarModule = (function () {
     }
 })();
 
+const HeaderModule = (function () {
+    const header = document.getElementById('banner-header');
+
+    // Active header if page is away from top
+    window.addEventListener('scroll', function () {
+        let y = Math.round(this.scrollY);
+
+        if (y > 0) {
+            header.classList.add('banner__header--scroll-active');
+        } else {
+            header.classList.remove("banner__header--scroll-active");
+        }
+    });
+})();
+
 const PreLoadAnimation = (function () {
     const preload = document.getElementById('preload');
-    const loadedContent = document.getElementById('loaded-content');
     const bannerTitleTop = document.querySelector('.js-banner__title-top');
     const bannerTitleBottom1 = document.querySelector('.js-banner__title-bottom-1');
     const bannerTitleBottom2 = document.querySelector('.js-banner__title-bottom-2');
@@ -103,50 +119,123 @@ const PreLoadAnimation = (function () {
         var timePoint = performance.now();
         if (timePoint < limitTime) {
             setTimeout(function () {
-                FadedAnimation();
+                (new PreloadVisibility()).Hidden();
                 decorateBannerTitleAnimate();
             }, (limitTime - timePoint));
         } else {
-            FadedAnimation();
+            (new PreloadVisibility()).Hidden();
             decorateBannerTitleAnimate();
         }
     });
 
-    function FadedAnimation() {
-        preload.classList.add('preload--hidden');
-        setTimeout(function () {
-            preload.style.display = 'none';
-        }, 1200)
+    // Make loading animation slowly faded
+    function PreloadVisibility() {
+        this.Hidden = function () {
+            preload.classList.add('preload--hidden');
+            setTimeout(function () {
+                preload.style.display = 'none';
+            }, 1200)
+        }
+
+        this.Active = function () {
+            preload.classList.remove('preload--hidden');
+            preload.style.display = 'block';
+        }
     }
 
+    // Start running Banner title text-animation
     function decorateBannerTitleAnimate() {
         bannerTitleTop.classList.add('banner__title-animation-bottom');
         bannerTitleBottom1.classList.add('banner__title-animation-left');
         bannerTitleBottom2.classList.add('banner__title-animation-right');
     }
+
+    App.PreloadVisibility = PreloadVisibility;
 })()
 
 const EmailService = (function () {
-    var modal = document.getElementById("modal-type-1");
-    var modalText = document.getElementById("email-message-text");
-    emailjs.init('user_Ubj9Yj81C14AvGgwmUkwf');
+    const userId = 'user_Ubj9Yj81C14AvGgwmUkwf';
+    const serviceId = 'service_ew59x8a';
+    const templateId = 'template_hxytvei';
+    const messageDisplayTime = 1800;
+    emailjs.init(userId);
 
     document.getElementById('contact-form').addEventListener('submit', function (event) {
         event.preventDefault();
-        // generate a five digit number for the contact_number variable
-        this.contact_number.value = Math.random() * 100000 | 0;
-        // Service_Id + template_Id
-        emailjs.sendForm('service_ew59x8a', 'template_hxytvei', this)
+
+        (new App.PreloadVisibility()).Active();
+
+        //Start Sending Email
+        emailjs.sendForm(serviceId, templateId, this)
             .then(function () {
-                modal.style.display = "block";
-                modalText.innerText = 'Thank you for sending me a message. I will reply soon';
-                setTimeout(function(){
-                    modal.style.display = "none"; 
-                }, 1500);
-            }, function (error) {
-                modalText.innerText = 'Some thing when wrong, please try again';
-            });
+        (new App.PreloadVisibility()).Hidden();
+
+        (new App.ModalVisibility()).Active();
+        App.SetModalText('Thank you for sending me a message. I will reply soon', App.MessageMode.Success);
+
+        setTimeout(function () {
+            (new App.ModalVisibility()).Hidden();
+        }, messageDisplayTime);
+        }, function (error) {
+            App.SetModalText('Some thing when wrong, please try again', App.MessageMode.Fail);
+        });
     });
+})()
+
+const ModalModule = (function () {
+    const modal = document.getElementById("modal-type-1");
+    const modalContent = document.getElementById("modal-content-type-1");
+    const modalText = document.getElementById("email-message-text");
+
+    const successIcon = document.querySelector('.js-success-icon');
+    const failIcon = document.querySelector('.js-fail-icon');
+    const errorIcon = document.querySelector('.js-error-icon');
+
+    const MessageMode = {
+        Success: 0,
+        Fail: 1,
+        Error: 2
+    }
+
+    function ModalVisibility() {
+        this.Hidden = function () {
+            modalContent.classList.remove('modal-show-animation')
+            modalContent.classList.add('modal-hide-animation');
+            setTimeout(function () {
+                modal.style.display = "none";
+            }, 500)
+        }
+
+        this.Active = function (messageMode) {
+            modal.style.display = "block";
+
+            modalContent.classList.remove('modal-hide-animation');
+            modalContent.classList.add('modal-show-animation');
+        }
+    }
+
+    function SetModalText(text, messageMode) {
+        modalText.innerText = text;
+
+        switch (messageMode) {
+            case MessageMode.Success:
+                successIcon.style.display = "block";
+                failIcon.style.display = "none";
+                errorIcon.style.display = "none";
+            case MessageMode.Fail:
+                successIcon.style.display = "none";
+                failIcon.style.display = "block";
+                errorIcon.style.display = "none";
+            case MessageMode.Error:
+                successIcon.style.display = "none";
+                failIcon.style.display = "none";
+                errorIcon.style.display = "block";
+        }
+    }
+
+    App.ModalVisibility = ModalVisibility;
+    App.SetModalText = SetModalText;
+    App.MessageMode = MessageMode;
 })()
 
 SmoothScroll({
@@ -178,7 +267,7 @@ AOS.init({
 
     // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
     offset: 120, // offset (in px) from the original trigger point
-    delay: 0, // values from 0 to 3000, with step 50ms
+    delay: 100, // values from 0 to 3000, with step 50ms
     duration: 1500, // values from 0 to 3000, with step 50ms
     easing: 'ease', // default easing for AOS animations
     once: false, // whether animation should happen only once - while scrolling down

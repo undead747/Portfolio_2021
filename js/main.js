cbf7c8d15bea58eaf52db690f8177ff8616b2a12
TechCare <narutominh747@gmail.com>
2022-02-08 15:17:23 +0700

    fix slide animation

--- js/main.js
+++ js/main.js
@@ -569,7 +569,7 @@ const OnLoadPage = (function () {
 
 SmoothScroll({
     frameRate: 150,
-    animationTime: 800,
+    animationTime: 700,
     stepSize: 100,
     pulseAlgorithm: 1,
     pulseScale: 4,
@@ -581,6 +581,7 @@ SmoothScroll({
     fixedBackground: 0
 });
 
+// You can also pass an optional settings object
 // below listed default settings
 AOS.init({
     // Global settings:
@@ -592,15 +593,15 @@ AOS.init({
     disableMutationObserver: false, // disables automatic mutations' detections (advanced)
     debounceDelay: 50, // the delay on debounce used while resizing window (advanced)
     throttleDelay: 99, // the delay on throttle used while scrolling the page (advanced)
-
-
+    
+  
     // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
     offset: 120, // offset (in px) from the original trigger point
-    delay: 100, // values from 0 to 3000, with step 50ms
-    duration: 1500, // values from 0 to 3000, with step 50ms
-    easing: 'ease', // default easing for AOS animations
-    once: false, // whether animation should happen only once - while scrolling down
+    delay: 0, // values from 0 to 3000, with step 50ms
+    duration: 800, // values from 0 to 3000, with step 50ms
+    easing: 'ease-in-out', // default easing for AOS animations
+    once: true, // whether animation should happen only once - while scrolling down
     mirror: false, // whether elements should animate out while scrolling past them
     anchorPlacement: 'top-bottom', // defines which position of the element regarding to window should trigger the animation
-
-});
+  
+  });
